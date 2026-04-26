e56eccf7951b226365801897f6288f096f413b53
CesarAntoniohuari <cesarantoniohuari@alu.frlp.utn.edu.ar>
2026-04-25 21:34:48 -0300

    fix(database): corrige sintaxis del nombre de la tabla y instruccion insert
    se corrige "insert miembros" por " insert members" y ";" para cerrarla instruccion

--- database/init.sql
+++ database/init.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS members (
     estado VARCHAR(20) DEFAULT 'activo'
 );
 
-INSERT INTO  miembros (nombre,apellido,legajo,feature,servicio,estado) VALUES
+INSERT INTO  members (nombre,apellido,legajo,feature,servicio,estado) VALUES
 ('Francina','Ruaro', '33278', 'feature01','coordinador/a del grupo','activo'),    
 ('Facundo','Pierrard', '31487', 'feature02','Frontend','activo'),        
 ('Ernesto Helvio','Ardenghi', '15446', 'feature03','Backend','activo'),    
-('Cesar Antonio','Huari', '32533', 'feature04','Base de datos','activo')
+('Cesar Antonio','Huari', '32533', 'feature04','Base de datos','activo');
